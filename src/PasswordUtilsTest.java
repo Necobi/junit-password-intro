1ec862c8c5f16594629252f370f4bee56e7446bd
Necobi <274595030+Necobi@users.noreply.github.com>
2026-04-27 11:51:51 -0700

    Made a couple of tests that tested the Alphanumberic method

--- src/PasswordUtilsTest.java
+++ src/PasswordUtilsTest.java
@@ -1,8 +1,69 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordUtilsTest {
     @Test
     void testDescribePasswordLengthShortPassword() {
-
+        //Arrange 
+        String password = "";
+        //Act - method you're testing
+        String actual = PasswordUtils.describePasswordLength(password);
+        //Assert (expected, actual)
+        assertEquals("short", actual);
     }
+
+    @Test
+    void testDescribePasswordLengthLongPassword(){
+        //Arrange
+        String password = "supercalifragilisticexpealidocious";
+        //Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        //Assert
+        assertEquals("long", actual);
+    }
+     @Test
+    void testDescribePasswordLongerThan40charsIsVeryLong(){
+        //Arrange
+        String password = "qwertyuiopasdfghjklzxcvbnm,asgybugytsmodnfjyh";
+        //Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        //Assert
+        assertEquals("very long", actual);
+    }
+    
+
+
+    @Test
+    void testDescribePasswordWorksWithPercentSign(){
+        //Arrange
+        String password = "ilovemydog123%"; //shouldn't work
+        //Act
+        boolean actual = PasswordUtils.isAlphanumeric(password);
+        //Assert
+        assertEquals(false, actual);
+    }
+
+      @Test
+    void testDescribePasswordWorksWithOnlyLetters(){
+        //Arrange
+        String password = "ilovemydog"; //should work
+        //Act
+        boolean actual = PasswordUtils.isAlphanumeric(password);
+        //Assert
+        assertEquals(true, actual);
+    }
+
+     @Test
+    void testDescribePasswordWorksWithOnlyNumbers(){
+        //Arrange
+        String password = "12345"; //should work
+        //Act
+        boolean actual = PasswordUtils.isAlphanumeric(password);
+        //Assert
+        assertEquals(true, actual);
+    }
+    
+
 }
+
